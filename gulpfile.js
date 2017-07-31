eae212982e134bb5a54175ad313de6606bb26b3c
Cameron Diver <cameron@resin.io>
2017-07-31 12:26:43 +0100

    Use tabs instead of spaces in gulpfile
    Change-type: patch
    Signed-off-by: Cameron Diver <cameron@resin.io>

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,21 @@ const tsProject = typescript.createProject('tsconfig.json');
 
 // Compile the TS sources
 gulp.task('typescript', () => {
-    tsProject.src()
-        .pipe(sourcemaps.init())
-        .pipe(tsProject()).on('error', gutil.log)
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(tsProject.options.outDir));
+	tsProject.src()
+	.pipe(sourcemaps.init())
+	.pipe(tsProject()).on('error', gutil.log)
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest(tsProject.options.outDir));
 });
 
 // Copy any pre-defined declarations
 gulp.task('copydecs', () => {
-    const decDirs = [];
-    tsProject.config.include.forEach((dir) => {
-        decDirs.push(`${dir.split('/')[0]}/**/*.d.ts`);
-    });
-    gulp.src(decDirs)
-        .pipe(gulp.dest(tsProject.options.declarationDir));
+	const decDirs = [];
+	tsProject.config.include.forEach((dir) => {
+		decDirs.push(`${dir.split('/')[0]}/**/*.d.ts`);
+	});
+	gulp.src(decDirs)
+	.pipe(gulp.dest(tsProject.options.declarationDir));
 });
 
 gulp.task('build', [ 'typescript', 'copydecs' ]);
